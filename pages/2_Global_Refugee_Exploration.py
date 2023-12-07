7aeee9a768a76d1de5b5c2bf424ce9d30b47df4e
MoukhikMisra <moukhikm@andrew.cmu.edu>
2023-12-07 10:47:12 -0500

    Edited filenames

---
+++ pages/2_Global_Refugee_Exploration.py
@@ -0,0 +1,146 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+st.title("Analysis of Global Refugee Dataset")
+#st.markdown("Placeholder markdown")
+
+dataset = pd.read_csv(filepath_or_buffer='./data/population.csv', delimiter= ',')
+
+
+st.header('Refugees under UNHCR\'s Mandate Over Years')
+
+st.write('The line chart below shows the change in the number of refugees under the UNHCR mandate over the years 1970-2021')
+
+df_years = dataset.groupby('Year')['Refugees under UNHCR\'s mandate'].sum().reset_index()
+
+start_year, end_year = st.slider('Select Year Range', min_value=int(df_years['Year'].min()), max_value=int(df_years['Year'].max()), value=(int(df_years['Year'].min()), int(df_years['Year'].max())))
+selected_year_range = df_years[(df_years['Year'] >= start_year) & (df_years['Year'] <= end_year)]
+
+fig = px.line(selected_year_range, x='Year', y='Refugees under UNHCR\'s mandate')
+st.plotly_chart(fig, use_container_width=True)
+
+st.write('')
+
+#
+
+number_of_countries1 = 5
+number_of_countries2 = 5
+
+st.header("Origin Country of Refugees")
+st.write('The bar chart below shows the countries producing the most refugees per year')
+#
+
+col1, col2 = st.columns(2)
+with col1:
+    number_of_countries1 = st.slider('Select the number of countries to display', 5, 20, 5)
+
+with col2:
+    y1= st.slider('Select Year', min_value=int(df_years['Year'].min()), max_value=int(df_years['Year'].max()), value=2021)
+
+
+df_refugeesfrom = dataset[dataset['Year'] == y1]
+df_refugeesfrom = df_refugeesfrom.groupby('Country of origin')['Refugees under UNHCR\'s mandate'].sum().reset_index()
+df_refugeesfrom = df_refugeesfrom.sort_values(by='Refugees under UNHCR\'s mandate', ascending=False)
+fig1 = px.bar(df_refugeesfrom.head(number_of_countries1), x='Country of origin', y='Refugees under UNHCR\'s mandate', title=f"Top {number_of_countries1} Origin Countries of Refugees in {y1}")
+st.plotly_chart(fig1, use_container_width=True)
+
+
+
+st.header("Country of Asylum")
+st.write('The bar chart below shows the countries where refugees have seeked Asylum per-year')
+
+col1, col2 = st.columns(2)
+with col1:
+    number_of_countries1 = st.slider('Select the number of countries', 5, 20, 5)
+
+with col2:
+    y2 = st.slider('Select Desired Year', min_value=int(df_years['Year'].min()), max_value=int(df_years['Year'].max()), value=int(df_years['Year'].max()))
+
+
+df_refugeesto= dataset[dataset['Year'] == y2]
+df_refugeesto = df_refugeesto.groupby('Country of asylum')['Refugees under UNHCR\'s mandate'].sum().reset_index()
+df_refugeesto = df_refugeesto.sort_values(by='Refugees under UNHCR\'s mandate', ascending=False)
+fig2 = px.bar(df_refugeesto.head(number_of_countries1), x='Country of asylum', y='Refugees under UNHCR\'s mandate', title=f"Top {number_of_countries1} Countries where Refugees have seeked Asylum in {y1}")
+st.plotly_chart(fig2, use_container_width=True)
+
+
+
+st.header("Country-wise Refugee Flows - A Year-wise View")
+
+col1, col2 = st.columns(2)
+
+
+df = dataset.copy()
+
+with col1:
+    year = st.slider('Desired Year for Flow', min_value=int(df['Year'].min()), max_value=int(df['Year'].max()), value=int(df['Year'].max()))
+    
+    
+
+df = df[df['Year'] == year]
+
+
+df = df[df['Refugees under UNHCR\'s mandate']>0]
+df['Log scaled Refugees under UNHCR\'s mandate'] = np.log(df['Refugees under UNHCR\'s mandate'])
+
+grouped_data = df.groupby(['Country of origin', 'Country of asylum'])['Refugees under UNHCR\'s mandate'].sum().reset_index()
+#grouped_data = grouped_data.head(20)
+
+all_countries = sorted(list(set(grouped_data['Country of origin']).union(set(grouped_data['Country of asylum']))))
+
+country_index = {country: idx for idx, country in enumerate(all_countries)}
+
+#selected_country_of_origin = st.selectbox('Select Country of Origin', all_countries)  # Replace with your country of choice
+
+
+    
+with col2:
+    max_flows = st.slider('Max Flows', min_value=0, max_value=20, value=10)
+
+
+selected_country_of_origin = st.selectbox('Select Country of Origin', all_countries, index=all_countries.index(all_countries[0] if "Afghanistan" not in all_countries else "Afghanistan"))
+
+# Filter and sort the data for the selected country
+filtered_data = grouped_data[grouped_data['Country of origin'] == selected_country_of_origin]
+sorted_data = filtered_data.sort_values(by='Refugees under UNHCR\'s mandate', ascending=False).head(max_flows)
+
+
+# Update the node and link information
+source = [country_index[selected_country_of_origin] for _ in range(len(sorted_data))]
+target = [country_index[asylum] for asylum in sorted_data['Country of asylum']]
+value = sorted_data['Refugees under UNHCR\'s mandate'].tolist()
+
+colors = px.colors.qualitative.Plotly
+link_colors = [colors[i % len(colors)] for i in range(len(value))]
+
+# Update the figure
+fig = go.Figure(data=[go.Sankey(
+    node=dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="black", width=0.5),
+        label=all_countries
+    ),
+    link=dict(
+        source=source,
+        target=target,
+        value=value,
+        color=link_colors
+    ))])
+
+fig.update_layout(title_text=f"Refugee Flows from {selected_country_of_origin} in {year}", font_size=15, height = 600)
+
+# Display the plot in Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+
+
---
+++ pages/3_Combined_Exploration.py
@@ -0,0 +1,242 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import altair as alt
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+    <h1 style='text-align: center; color: white;'>Combined Analysis</h1>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:32px !important;
+        color: yellow !important;
+    }
+    </style>
+    
+    <p class='big-font'>Correlation Analysis</p>
+    """, unsafe_allow_html=True)
+st.write("This platform enables users to explore the intricate relationship between the frequency of terrorist incidents and the resulting refugee flows, both from and into a selected country, over a specified time period.")
+st.write("Select a country and time period to view the correlation between the frequency of attacks and the patterns of refugee flow.")
+st.write("Accompanying each plot, correlation coefficients provide a statistical measure of the relationship between these two phenomena, offering insights into how terrorism may drive refugee flows or how an influx of refugees may correlate with the frequency of attacks.")
+
+df_terror_refugee_dest = pd.read_csv("data/terror_refugee_dest.csv")
+df_terror_refugee_origin = pd.read_csv("data/terror_refugee_origin.csv")
+df_refugee_grouped = pd.read_csv("data/refugee_grouped.csv")
+df_terror_grouped = pd.read_csv("data/terror_grouped.csv")
+
+
+if 'selected_country' not in st.session_state or st.session_state.selected_country not in df_terror_refugee_dest["country"].unique():
+    st.session_state.selected_country = sorted(list(df_terror_refugee_dest["country"].unique()))[0]
+
+st.session_state.selected_country = st.selectbox("Select a country", sorted(list(df_terror_refugee_dest["country"].unique())))
+# Choose a country
+df_terror_refugee_origin = df_terror_refugee_origin[df_terror_refugee_origin['country'] == st.session_state.selected_country]
+
+min_year = max(df_terror_refugee_origin["iyear"].min(), df_terror_refugee_dest["iyear"].min())
+max_year = min(df_terror_refugee_origin["iyear"].max(), df_terror_refugee_dest["iyear"].max())
+
+filtered_years = st.slider("Filter for years", min_value=min_year, max_value=max_year, value=(min_year, max_year))
+
+df_terror_refugee_dest = df_terror_refugee_dest[(df_terror_refugee_dest["iyear"] <= max(filtered_years)) & (df_terror_refugee_dest["iyear"] >= min(filtered_years))]
+df_terror_refugee_origin = df_terror_refugee_origin[(df_terror_refugee_origin["iyear"] <= max(filtered_years)) & (df_terror_refugee_origin["iyear"] >= min(filtered_years))]
+
+# Calculate correlation coefficient
+corr_origin = df_terror_refugee_origin['Attack Count'].corr(df_terror_refugee_origin['Refugee Count (origin)'])
+corr_origin = round(corr_origin, 4)
+
+origin_base = alt.Chart(df_terror_refugee_origin).encode(
+    alt.X('iyear:Q', axis=alt.Axis(title='Year', format='')),
+)
+
+origin_line1 = origin_base.mark_line(color='red').encode(
+    alt.Y('Attack Count:Q', axis=alt.Axis(title='Attack Count')).axis(titleColor='red')
+)
+
+origin_line2 = origin_base.mark_line(color='green').encode(
+    alt.Y('Refugee Count (origin):Q', axis=alt.Axis(title='Refugee Count (origin)')).axis(titleColor='green')
+)
+
+chart1 = alt.layer(origin_line1, origin_line2).resolve_scale(
+    y = 'independent'
+).properties(
+    title="Origin Data Correlation"
+)
+
+# Choose a country
+df_terror_refugee_dest = df_terror_refugee_dest[df_terror_refugee_dest['country'] == st.session_state.selected_country]
+
+# Calculate correlation coefficient
+corr_dest = df_terror_refugee_dest['Attack Count'].corr(df_terror_refugee_dest['Refugee Count (destination)'])
+corr_dest = round(corr_dest, 4)
+
+dest_base = alt.Chart(df_terror_refugee_dest).encode(
+    alt.X('iyear:Q', axis=alt.Axis(title='Year', format='')),
+)
+
+dest_line1 = dest_base.mark_line(color='red').encode(
+    alt.Y('Attack Count:Q', axis=alt.Axis(title='Attack Count')).axis(titleColor='red')
+)
+
+dest_line2 = dest_base.mark_line(color='cyan').encode(
+    alt.Y('Refugee Count (destination):Q', axis=alt.Axis(title='Refugee Count (destination)')).axis(titleColor='cyan')
+)
+
+chart2 = alt.layer(dest_line1, dest_line2).resolve_scale(
+    y = 'independent'
+).properties(
+    title="Destination Data Correlation"
+)
+
+country_selected = st.session_state.selected_country
+
+# Origin
+col1, col2 = st.columns([3, 1])
+with col1:
+    st.subheader(f"Terrorist attacks and refugees fleeing from {country_selected}")
+    st.altair_chart(chart1, use_container_width=True)
+with col2:
+    delta_text_origin = ""
+    if corr_origin >= 0.7:
+        delta_text_origin = "Highly correlated"
+    elif corr_origin >= 0.5:
+        delta_text_origin = "Moderately correlated"
+    else:
+        delta_text_origin = "Weakly correlated"
+    
+    if corr_origin < 0:
+        delta_text_origin = "-" + delta_text_origin
+    else:
+        delta_text_origin = "+" + delta_text_origin
+
+    st.metric(label="Correlation Coefficient", value=corr_origin, delta=delta_text_origin)
+
+# Destination
+col3, col4 = st.columns([3, 1])
+with col3:
+    st.subheader(f"Terrorist attacks and refugees coming to {country_selected}")
+    st.altair_chart(chart2, use_container_width=True)
+with col4:
+    delta_text_dest = ""
+    if corr_dest >= 0.7:
+        delta_text_dest = "Highly correlated"
+    elif corr_dest >= 0.5:
+        delta_text_dest = "Moderately correlated"
+    else:
+        delta_text_dest = "Weakly correlated"
+    
+    if corr_dest < 0:
+        delta_text_dest = "-" + delta_text_dest
+    else:
+        delta_text_dest = "+" + delta_text_dest
+
+    st.metric(label="Correlation Coefficient", value=corr_dest, delta=delta_text_dest)
+
+# ---------------------------------------------------------------------------------------------------------------------------------
+
+st.markdown("""
+    <style>
+    .big-font {
+        font-size:32px !important;
+        color: yellow !important;
+    }
+    </style>
+    
+    <p class='big-font'>Refugees Flows</p>
+    """, unsafe_allow_html=True)
+
+df_refugee_grouped_origin = df_refugee_grouped[df_refugee_grouped['Country of origin'] == st.session_state.selected_country]
+df_refugee_grouped_origin = df_refugee_grouped_origin[(df_refugee_grouped_origin["Year"] <= max(filtered_years)) & (df_refugee_grouped_origin["Year"] >= min(filtered_years))]
+
+# Get the most popular destination for refugees
+df_refugee_grouped_origin = df_refugee_grouped_origin.groupby(["Country of asylum"]).sum().sort_values(by=["Refugees under UNHCR's mandate"], ascending=False).reset_index()
+most_popular_dest = df_refugee_grouped_origin.iloc[0]["Country of asylum"]
+
+# Calculate the average attackas per year of that country in that time period
+df_terror_grouped_dest = df_terror_grouped[df_terror_grouped['country'] == most_popular_dest]
+df_terror_grouped_dest = df_terror_grouped_dest[(df_terror_grouped_dest["iyear"] <= max(filtered_years)) & (df_terror_grouped_dest["iyear"] >= min(filtered_years))]
+avg_attacks_dest = df_terror_grouped_dest["count"].mean()
+avg_attacks_dest = int(avg_attacks_dest)
+
+# Calculate the average attackas per year of that country in that time period
+df_terror_grouped_origin = df_terror_grouped[df_terror_grouped['country'] == st.session_state.selected_country]
+df_terror_grouped_origin = df_terror_grouped_origin[(df_terror_grouped_origin["iyear"] <= max(filtered_years)) & (df_terror_grouped_origin["iyear"] >= min(filtered_years))]
+avg_attacks_origin = df_terror_grouped_origin["count"].mean()
+avg_attacks_origin = int(avg_attacks_origin)
+
+st.markdown("""
+    <style>
+    .highlight {
+        font-size:22px !important;
+        color: green !important;
+    }
+    </style>
+    
+    Refugees fleeing from <span class='highlight'>""" + country_selected + """</span>""" + """ are most likely to flee to <span class='highlight'>""" + most_popular_dest + """</span> in search of asylum"""
+    , unsafe_allow_html=True)
+
+year_range_str = str(min(filtered_years)) + " - " + str(max(filtered_years))
+
+st.markdown("""
+    <style>
+    .highlight {
+        font-size:22px !important;
+        color: green !important;
+    }
+    .number {
+        font-size:22px !important;
+        color: red !important;
+    }
+    </style>
+    
+    Compared to <span class='highlight'>""" + country_selected + """</span> that has <span class='number'>""" + str(avg_attacks_origin) + """</span> attacks per year on average during <span class='number'>""" + year_range_str + """</span>, <span class='highlight'>""" + most_popular_dest + """</span> has <span class='number'>""" + str(avg_attacks_dest) + """</span> attacks per year on average during the same time period"""
+    , unsafe_allow_html=True)
+
+df_refugee_grouped_dest = df_refugee_grouped[df_refugee_grouped['Country of asylum'] == st.session_state.selected_country]
+df_refugee_grouped_dest = df_refugee_grouped_dest[(df_refugee_grouped_dest["Year"] <= max(filtered_years)) & (df_refugee_grouped_dest["Year"] >= min(filtered_years))]
+
+# Get the most popular destination for refugees
+df_refugee_grouped_dest = df_refugee_grouped_dest.groupby(["Country of origin"]).sum().sort_values(by=["Refugees under UNHCR's mandate"], ascending=False).reset_index()
+most_popular_origin = df_refugee_grouped_dest.iloc[0]["Country of origin"]
+
+avg_attacks_dest = avg_attacks_origin
+
+# Calculate the average attackas per year of that country in that time period
+df_terror_grouped_origin = df_terror_grouped[df_terror_grouped['country'] == most_popular_origin]
+df_terror_grouped_origin = df_terror_grouped_origin[(df_terror_grouped_origin["iyear"] <= max(filtered_years)) & (df_terror_grouped_origin["iyear"] >= min(filtered_years))]
+avg_attacks_origin = df_terror_grouped_origin["count"].mean()
+avg_attacks_origin = int(avg_attacks_origin)
+
+st.markdown("""
+    <style>
+    .highlight {
+        font-size:22px !important;
+        color: green !important;
+    }
+    </style>
+    
+    Refugees fleeing to <span class='highlight'>""" + country_selected + """</span>""" + """ in search of asylum are most likely from <span class='highlight'>""" + most_popular_origin + """</span>"""
+    , unsafe_allow_html=True)
+
+year_range_str = str(min(filtered_years)) + " - " + str(max(filtered_years))
+
+st.markdown("""
+    <style>
+    .highlight {
+        font-size:22px !important;
+        color: Teal   !important;
+    }
+    .number {
+        font-size:22px !important;
+        color: Coral  !important;
+    }
+    </style>
+    
+    Compared to <span class='highlight'>""" + country_selected + """</span> that has <span class='number'>""" + str(avg_attacks_dest) + """</span> attacks per year on average during <span class='number'>""" + year_range_str + """</span>, <span class='highlight'>""" + most_popular_origin + """</span> has <span class='number'>""" + str(avg_attacks_origin) + """</span> attacks per year on average during the same time period"""
+    , unsafe_allow_html=True)
