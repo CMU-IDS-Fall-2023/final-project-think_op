527b2950616498ee18ff6f9c8cb112b184b0de58
Prachiti Garge <prachiti4040@gmail.com>
2023-12-07 09:40:31 -0500

    Refactored the GTD dashboard to have the filters on the sidebar. Text inserted.

--- pages/Global_Terrorism.py
+++ pages/Global_Terrorism.py
@@ -12,73 +12,106 @@ import plotly.express as px
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-st.title("Analysis of the Global Terrorism Database")
-st.header("Overall Statistics")
+st.title("Exploration of the Global Terrorism Database")
+
+st.write("Use the filters on the left sidebar to explore the global terrorism database. All the plots are filtered on the years. Changing the time range will reset the victim country and perpetrator terrorist group filters to 'All'. The current version of the Global Terrorism Database only has data till mid-2021.")
 
 pre_df = pd.read_csv('data/gtd_1970_2021_selective.csv')
 
 pre_df = pre_df.dropna(subset=['latitude', 'longitude'])
 
-filtered_years1 = st.slider("Filter on years for all plots", min_value=pre_df["iyear"].min(), max_value=pre_df["iyear"].max(), value=(1970, 2021))
-
-df = pre_df[(pre_df["iyear"] <= max(filtered_years1)) & (pre_df["iyear"] >= min(filtered_years1))]
-
-#st.map(pre_df, latitude='latitude', longitude='longitude')
-
-# DONE: Line graph for terrorist incidents over years (Filter for years and country)
+filtered_years0 = st.sidebar.slider("Filter by years for all plots", min_value=pre_df["iyear"].min(), max_value=pre_df["iyear"].max(), value=(1970, 2021))
+df = pre_df[(pre_df["iyear"] <= max(filtered_years0)) & (pre_df["iyear"] >= min(filtered_years0))]
 
 all_countries_list = sorted(list(df["country_txt"].unique()) + ['All'])
 
-filtered_country2 = st.selectbox("Filter by Country", all_countries_list,index=all_countries_list.index("All"))
-
-
-#df_fil2 = df[(df["iyear"] <= max(filtered_years1)) & (df["iyear"] >= min(filtered_years1))]
-# count2_1 = df_fil2.groupby('iyear').country_txt.count().reset_index()
-#count2_2 = df_fil2.groupby('iyear').country_txt.nunique().reset_index()
-# count2_2.rename(columns={'country_txt': 'ncountries'}, inplace=True)
-# df_fil2_2 = pd.merge(count2_1, count2_2, on = ["iyear","iyear"])
-# df_fil2_2['mean_per_year'] = df_fil2_2.country_txt/df_fil2_2.ncountries
-#st.table(count2_2)
-if filtered_country2 != 'All':
-    df_fil2 = df[(df.country_txt == filtered_country2)]
+filtered_country0 = st.sidebar.selectbox("Filter by Country (where applicable)", all_countries_list,index=all_countries_list.index("All"))
+country_filter = False
+if filtered_country0 != 'All':
+    df_c = df[(df.country_txt == filtered_country0)]
+    df_c_g = df_c
+    country_insert = filtered_country0
+    
 else:
-    df_fil2=df
+    df_c = df
+    df_c_g = df
+    country_insert = "the world"
+    
+all_gname_list = sorted(list(df["gname"].unique()) + ['All'])
 
-line1 = alt.Chart(df_fil2).mark_line().encode(
+filtered_gname0 = st.sidebar.selectbox("Filter by Terrorist group (where applicable)", all_gname_list,index=all_gname_list.index("All"))
+
+if filtered_gname0 != 'All':
+    df_g = df[(df.gname == filtered_gname0)]
+    df_c_g = df_c_g[(df_c_g.gname == filtered_gname0)]
+    tg_insert = filtered_gname0
+else:
+    df_g = df
+    tg_insert = "terrorists"
+
+num_killed=df_c_g['nkill'].sum()
+
+st.write("Time filter has been set to:", min(filtered_years0),"-",max(filtered_years0))
+st.write("Country filter has been set to:", filtered_country0 )
+st.write("Terrorist Group filter has been set to:", filtered_gname0 )
+
+# DONE: Line graph for terrorist incidents over years (Filter for years and country)
+
+st.header("Overall Statistics")
+st.subheader("Terrorist incidents over years (filtered on country)")
+
+line1 = alt.Chart(df_c).mark_line().encode(
    alt.X("iyear:O",bin=False,title="Year"),
    alt.Y("count()",title="Number of Terrorist incidents"),
-).properties(title="Terrorist incidents over years for specific countries").configure_title(anchor='start')
-
+)
 st.altair_chart(line1, use_container_width=True, theme="streamlit")
+
+st.write(num_killed, " people were killed in ", country_insert, " by ", tg_insert," in the time frame ",min(filtered_years0),"-",max(filtered_years0),".")
+st.write("There was a sharp increase in the number of terrorist incidents in the past decade.")
+
+#DONE: Line plot with nkill filtered by gname
+st.subheader("Fatalities over the years (filtered by country and terrorist group)")
+
+df_perp2 = df_c_g.groupby(['iyear'])['nkill'].sum().reset_index()
+
+#st.table(df_perp2)
+line2 = alt.Chart(df_perp2).mark_line().encode(
+   alt.X("iyear:O",bin=False,title="Year"),
+   alt.Y("nkill",title="Number of People killed"),
+)
+st.altair_chart(line2, use_container_width=True, theme="streamlit")
+
+#DONE Map with nkill (Filter on year)
+st.subheader("Geospatial mapping of Number of incidents (filtered by country and terrorist group)")
+
+st.map(df_c_g, latitude='latitude', longitude='longitude')
+
+
 
 st.header("Targets of Attacks")
 
 #DONE: Bar plot for top countries with highest number of attacks (Filter on number and year range)
+st.subheader("Most victimised countries (filtered on number of values shown and terrorist group)")
+
 number_of_values1 = st.number_input("Number of top victim countries (1-30):",
                             min_value=1,
                             max_value=30,
                             value=10,
                             step=1)
-df_vic1= df.groupby(['country_txt'])['country_txt'].count().sort_values(ascending=False).head(number_of_values1)
+df_vic1= df_g.groupby(['country_txt'])['country_txt'].count().sort_values(ascending=False).head(number_of_values1)
 df_vic1 = df_vic1.rename("count_c").reset_index()
 
 bar1 = alt.Chart(df_vic1).mark_bar().encode(
    alt.X("count_c",bin=False,title="Number of terrorist attacks"),
    alt.Y("country_txt",title="Victim Country",sort=alt.EncodingSortField(field="country_txt", op="count", order='descending')),
-).properties(title="Terrorist incidents over years for specific countries").configure_title(anchor='start')
-
+)
 st.altair_chart(bar1, use_container_width=True, theme="streamlit")
 
 
 #DONE: Sunburst on number of attacks
+st.subheader("Types of targets (filtered by country and terrorist group)")
 
-filtered_country4 = st.selectbox("Filter on Country", all_countries_list,index=all_countries_list.index("All"))
-
-if filtered_country4 != 'All':
-    df_vic = df[(df.country_txt == filtered_country4)]
-else:
-    df_vic=df
-df_vic = df_vic.groupby(['targtype1_txt','targsubtype1_txt'])['targtype1_txt'].count().to_frame()
+df_vic = df_c.groupby(['targtype1_txt','targsubtype1_txt'])['targtype1_txt'].count().to_frame()
 df_vic = df_vic.rename(columns={'targtype1_txt':'Count'}).reset_index()
 
 fig_target = px.sunburst(
@@ -96,48 +129,31 @@ st.plotly_chart(fig_target)
 st.header("Perpetrators of Attacks")
 
 #DONE: Bar plot for top perpetrator organizations with highest number of attacks (Filter on number and year range)
+st.subheader("Most active terrorist groups (filtered on number of values shown and country)")
 
-number_of_values2 = st.number_input("Number of top perpetrator organizations (1-30):",
+number_of_values2 = st.number_input("Number of top perpetrator groups (1-30):",
                             min_value=1,
                             max_value=30,
                             value=10,
                             step=1)
-df_perp1= df.groupby(['gname'])['gname'].count().sort_values(ascending=False).head(number_of_values1)
+df_perp1= df_c.groupby(['gname'])['gname'].count().sort_values(ascending=False).head(number_of_values1)
 df_perp1 = df_perp1.rename("count_org").reset_index()
 
 bar2 = alt.Chart(df_perp1).mark_bar().encode(
    alt.X("count_org",bin=False,title="Number of terrorist attacks"),
    alt.Y("gname",title="Perpetrator Organization",sort=alt.EncodingSortField(field="gname", op="count", order='descending')),
-).properties(title="Terrorist incidents over years by specific organizations").configure_title(anchor='start')
-
+)
 st.altair_chart(bar2, use_container_width=True, theme="streamlit")
 
-#DONE: Line plot with nkill filtered by gname
-all_gname_list = sorted(list(df["gname"].unique()) + ['All'])
-
-filtered_gname1 = st.selectbox("Filter by Terrorist organization", all_gname_list,index=all_gname_list.index("All"))
-
-if filtered_gname1 != 'All':
-    df_perp2 = df[(df.gname == filtered_gname1)]
-else:
-    df_perp2=df
-
-df_perp2 = df_perp2.groupby(['iyear'])['nkill'].sum().reset_index()
-
-#st.table(df_perp2)
-line2 = alt.Chart(df_perp2).mark_line().encode(
-   alt.X("iyear:O",bin=False,title="Year"),
-   alt.Y("nkill",title="Number of People killed"),
-).properties(title="Fatalities over the years").configure_title(anchor='start')
-
-st.altair_chart(line2, use_container_width=True, theme="streamlit")
-
 #DONE: Word cloud
+st.subheader("Word cloud on motive of attack(filtered by country and terrorist group)")
+
+
 motive_words = ''
 stopwords = set(STOPWORDS)
  
 # iterate through the csv file
-for val in df.motive:
+for val in df_c_g.motive:
      
     # typecaste each val to string
     val = str(val)
@@ -176,21 +192,23 @@ st.image(wordcloud_image, use_column_width=True)
 st.header("Details of Attacks")
 
 #DONE Bar plot for top types of attacks by number of casualties (Filter on number and year range)
+st.subheader("Types of attacks (filtered by country and terrorist group)")
 
-df_perp3= df.groupby('attacktype1_txt')['nkill'].count().sort_values(ascending=False).head(10)
+df_perp3= df_c_g.groupby('attacktype1_txt')['nkill'].count().sort_values(ascending=False).head(10)
 df_perp3 = df_perp3.rename("num_fatalties").reset_index()
 
 bar3 = alt.Chart(df_perp3).mark_bar().encode(
    alt.X("num_fatalties",bin=False,title="Number of terrorist attacks"),
    alt.Y("attacktype1_txt",title="Major attack type",sort=alt.EncodingSortField(field="gname", op="count", order='descending')),
-).properties(title="Terrorist incidents over years by specific organizations").configure_title(anchor='start')
+)
 
 st.altair_chart(bar3, use_container_width=True, theme="streamlit")
 
 
 #DONE: Sunburst on types of weapons
+st.subheader("Types of weapons used (filtered by country and terrorist group)")
 
-df_weap = df.groupby(['weaptype1_txt','weapsubtype1_txt'])['weaptype1_txt'].count().to_frame()
+df_weap = df_c_g.groupby(['weaptype1_txt','weapsubtype1_txt'])['weaptype1_txt'].count().to_frame()
 df_weap = df_weap.rename(columns={'weaptype1_txt':'Count'}).reset_index()
 fig_weap = px.sunburst(
     df_weap, path = ['weaptype1_txt', 'weapsubtype1_txt'],
@@ -204,19 +222,3 @@ fig_weap.update_layout(
 
 st.plotly_chart(fig_weap)
 
-st.header("Geospatial mapping of Casualties")
-
-#DONE Map with nkill (Filter on year)
-filtered_gname2 = st.selectbox("Filter for Terrorist organization", all_gname_list,index=all_gname_list.index("All"))
-
-if filtered_gname2 != 'All':
-    df_map = df[(df.gname == filtered_gname2)]
-else:
-    df_map=df
-    
-filtered_countries5 = st.selectbox("Filter for Victim Country", all_countries_list,index=all_countries_list.index("All"))
-
-if filtered_countries5 != 'All':
-    df_map = df_map[(df_map.country_txt == filtered_countries5)]
-
-st.map(df_map, latitude='latitude', longitude='longitude',size='nkill'*10)
